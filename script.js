591502ffa59ef5df812e23d8b0942c44e34abc0a
Samuehhh <samueldelatorre2517@gmail.com>
2025-01-31 19:47:44 +0100

    avance tarde 31

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('btnApertura').addEventListener('click', () => {
+    let contenedorExperiencia = document.querySelector('.contenedor-experiencia');
+    contenedorExperiencia.classList.toggle('activo');
+});
